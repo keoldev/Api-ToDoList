51863344efdb71da7d7a37a0b73ac5e624c800c2
Keynzu <keynerolivo@gmail.com>
2023-03-08 06:10:49 -0500

    add credentials to CORS

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-from fastapi import FastAPI, Body, Query, Request
+from fastapi import FastAPI, Body, Request
 from db import insert_task, get_tasks, update_status, delete_task
 from models import CreateTaskModel, Status
 from uuid import uuid4
@@ -9,7 +9,7 @@ from fastapi.middleware.cors import CORSMiddleware
 
 app = FastAPI()
 handler = Mangum(app)
-methods=[
+methods = [
     "OPTIONS",
     "GET",
     "POST",
@@ -17,7 +17,7 @@ methods=[
     "DELETE"
 ]
 
-headers=[
+headers = [
     "Content-Type",
     "X-Amz-Date",
     "Authorization",
@@ -31,42 +31,44 @@ origins = [
 app.add_middleware(
     CORSMiddleware,
     allow_origins=origins,
+    allow_credentials=True,
     allow_methods=methods,
     allow_headers=headers,
 )
 
+
 def decode(token: str):
     return jwt.decode(token, algorithms=["RS256"], options={"verify_signature": False})
-    
 
 
 @app.post('/')
 def create_task(request: Request, task_data: CreateTaskModel = Body(...)):
-    token=request.headers.get("Authorization")
+    token = request.headers.get("Authorization")
     insert_task(user_id=decode(token)["sub"], task=task_data.task)
     return {"message": "task created successfully"}
 
 
 @app.get('/')
 def read_task(request: Request):
-    token=request.headers.get("Authorization")
-    tasks_list=get_tasks(user_id=decode(token)["sub"])
-    return {"tasks" : tasks_list}
+    token = request.headers.get("Authorization")
+    tasks_list = get_tasks(user_id=decode(token)["sub"])
+    return {"tasks": tasks_list}
+
 
 @app.put('/')
 def update_task(request: Request, task_id: str, status: Status):
-    token=request.headers.get("Authorization")
+    token = request.headers.get("Authorization")
     if status.value == 'incomplete':
-        status_updated= 'completed'
-    else: status_updated = 'incomplete'
-    
+        status_updated = 'completed'
+    else:
+        status_updated = 'incomplete'
+
     update_status(task_id, status_updated, user_id=decode(token)["sub"])
-    return {"message":"successfully!!!"}
+    return {"message": "successfully!!!"}
 
 
 @app.delete('/')
 def remove_task(request: Request, task_id: str):
-    token=request.headers.get("Authorization")
+    token = request.headers.get("Authorization")
     delete_task(task_id, user_id=decode(token)["sub"])
-    return {"message":"task deleted successfully!!!"}
-
+    return {"message": "task deleted successfully!!!"}
