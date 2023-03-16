980754847b9b568e55b20e33d1c4df4449acc638
Keynzu <keynerolivo@gmail.com>
2023-03-16 02:45:11 -0500

    Update db environment variable

--- db.py
+++ db.py
@@ -7,7 +7,7 @@ db_client = boto3.client('dynamodb')
 
 def insert_task(user_id: str, task: str):
     db_client.put_item(
-        TableName=config('TABLE_NAME'),
+        TableName=config('DDB_TABLE_NAME'),
         Item={
             "user_id": {"S": user_id},
             "task_id": {"S": str(uuid.uuid4())},
@@ -19,7 +19,7 @@ def insert_task(user_id: str, task: str):
 
 def get_tasks(user_id: str):
     response = db_client.query(
-        TableName=config('TABLE_NAME'),
+        TableName=config('DDB_TABLE_NAME'),
         KeyConditionExpression='user_id= :user_id',
         ExpressionAttributeValues={
             ":user_id": {"S": user_id}
@@ -29,7 +29,7 @@ def get_tasks(user_id: str):
 
 def update_status(user_id: str, task_id: str, status: str ):
     response=db_client.update_item(
-        TableName=config('TABLE_NAME'),
+        TableName=config('DDB_TABLE_NAME'),
         Key={
             "user_id": {"S": user_id} ,
             "task_id": {"S": task_id}
@@ -45,7 +45,7 @@ def update_status(user_id: str, task_id: str, status: str ):
 
 def delete_task(user_id: str, task_id: str):
     response=db_client.delete_item(
-        TableName=config('TABLE_NAME'),
+        TableName=config('DDB_TABLE_NAME'),
         Key={
             "user_id": {"S": user_id} ,
             "task_id": {"S": task_id}
